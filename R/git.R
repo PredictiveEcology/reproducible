c52f1d71d8f45444b1d0ac69e74da964bc4b58a3
Alex Chubaty <alex.chubaty@gmail.com>
2017-06-14 16:22:39 -0600

    move 'robocopy' to new file 'file-copy.R'

--- R/git.R
+++ R/git.R
@@ -94,18 +94,3 @@ checkoutDev <- function(checkoutCondition) {
     remote_set_url(checkoutCondition$repo, "origin", url = checkoutCondition$sshURL)
   }
 }
-
-#' Use 'RoboCopy' on Windows to intelligently copy files
-#'
-#' @param from source directory
-#' @param to   destination directory
-#' @param options RoboCopy options (default \code{c("xo", "e")}).
-#' @param files files to copy
-robocopy <- function(from, to, options = c("xo", "e"), files) {
-  if (Sys.info()["sysname"] == "Windows") {
-    system(paste("robocopy", normalizePath(from),
-                 normalizePath(to), paste(paste0("/", options), collapse = " ")))
-  } else {
-    stop("RoboCopy is only available on Windows. On other operating systems, use 'rsync' if it's available instead.")
-  }
-}
