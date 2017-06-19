459754786b085da9dbf61eb01e2eee70a20a2cf8
Eliot McIntire <eliotmcintire@gmail.com>
2017-06-18 22:29:45 -0700

    test-cache.R

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -1,146 +1,3 @@
-test_that("test cache", {
-  library(igraph)
-  tmpdir <- file.path(tempdir(), "testCache") %>% checkPath(create = TRUE)
-  on.exit(unlink(tmpdir, recursive = TRUE), add = TRUE)
-  try(clearCache(tmpdir), silent = TRUE)
-
-  # Example of changing parameter values
-  mySim <- simInit(
-    times = list(start = 0.0, end = 1.0, timeunit = "year"),
-    params = list(
-      .globals = list(stackName = "landscape", burnStats = "nPixelsBurned"),
-      # Turn off interactive plotting
-      fireSpread = list(.plotInitialTime = NA),
-      caribouMovement = list(.plotInitialTime = NA),
-      randomLandscapes = list(.plotInitialTime = NA)
-    ),
-    modules = list("randomLandscapes", "fireSpread", "caribouMovement"),
-    paths = list(modulePath = system.file("sampleModules", package = "SpaDES"),
-                 outputPath = tmpdir,
-                 cachePath = tmpdir),
-    # Save final state of landscape and caribou
-    outputs = data.frame(objectName = c("landscape", "caribou"),
-                         stringsAsFactors = FALSE)
-  )
-
-  set.seed(1123)
-  sims <- experiment(mySim, replicates = 2, cache = TRUE)
-  out <- showCache(sims[[1]])
-  expect_output(print(out), "cacheId")
-  expect_true(NROW(out) == 16) # will become 15 with new experiment caching stuff
-  clearCache(sims[[1]])
-  out <- showCache(sims[[1]])
-  expect_true(NROW(out) == 0)
-})
-
-test_that("test event-level cache", {
-  library(igraph)
-  tmpdir <- file.path(tempdir(), "testCache") %>% checkPath(create = TRUE)
-  on.exit(unlink(tmpdir, recursive = TRUE), add = TRUE)
-  try(clearCache(tmpdir), silent = TRUE)
-
-  # Example of changing parameter values
-  mySim <- simInit(
-    times = list(start = 0.0, end = 1.0, timeunit = "year"),
-    params = list(
-      .globals = list(stackName = "landscape", burnStats = "nPixelsBurned"),
-      # Turn off interactive plotting
-      fireSpread = list(.plotInitialTime = NA),
-      caribouMovement = list(.plotInitialTime = NA),
-      randomLandscapes = list(.plotInitialTime = NA, .useCache = "init")
-    ),
-    modules = list("randomLandscapes", "fireSpread", "caribouMovement"),
-    paths = list(modulePath = system.file("sampleModules", package = "SpaDES"),
-                 outputPath = tmpdir,
-                 cachePath = tmpdir),
-    # Save final state of landscape and caribou
-    outputs = data.frame(objectName = c("landscape", "caribou"),
-                         stringsAsFactors = FALSE)
-  )
-
-  set.seed(1123)
-  #expect_true(!grepl(pattern = "Using cached copy of init event in randomLandscapes module",
-  #                   capture_messages(sims <- spades(Copy(mySim), notOlderThan = Sys.time()))))
-  sims <- spades(Copy(mySim), notOlderThan = Sys.time()) ## TO DO: fix this test
-  landscapeMaps1 <- raster::dropLayer(sims$landscape, "Fires")
-  fireMap1 <- sims$landscape$Fires
-
-  mess1 <- capture_messages(sims <- spades(Copy(mySim)))
-  expect_true(any(grepl(pattern = "Using cached copy of init event in randomLandscapes module", mess1)))
-  landscapeMaps2 <- raster::dropLayer(sims$landscape, "Fires")
-  fireMap2 <- sims$landscape$Fires
-
-  # Test that cached part comes up identical in both (all maps but Fires),
-  #   but non-cached part are different (Fires should be different because stochastic)
-  expect_equal(landscapeMaps1, landscapeMaps2)
-  expect_false(isTRUE(suppressWarnings(all.equal(fireMap1, fireMap2))))
-
-  clearCache(sims)
-})
-
-test_that("test module-level cache", {
-  library(igraph)
-  tmpdir <- file.path(tempdir(), "testCache") %>% checkPath(create = TRUE)
-  on.exit(unlink(tmpdir, recursive = TRUE), add = TRUE)
-
-  tmpfile <- tempfile(fileext = ".pdf")
-  file.create(tmpfile)
-  tmpfile <- normPath(tmpfile)
-  try(clearCache(tmpdir), silent = TRUE)
-
-  # Example of changing parameter values
-  times <- list(start = 0.0, end = 1.0, timeunit = "year")
-  mySim <- simInit(
-    times = times,
-    params = list(
-      .globals = list(stackName = "landscape", burnStats = "nPixelsBurned"),
-      # Turn off interactive plotting
-      fireSpread = list(.plotInitialTime = NA),
-      caribouMovement = list(.plotInitialTime = NA),
-      randomLandscapes = list(.plotInitialTime = times$start, .useCache = TRUE)
-    ),
-    modules = list("randomLandscapes", "fireSpread", "caribouMovement"),
-    paths = list(modulePath = system.file("sampleModules", package = "SpaDES"),
-                 outputPath = tmpdir,
-                 cachePath = tmpdir),
-    # Save final state of landscape and caribou
-    outputs = data.frame(objectName = c("landscape", "caribou"), stringsAsFactors = FALSE)
-  )
-
-  set.seed(1123)
-  pdf(tmpfile)
-  #expect_true(!grepl(pattern = "Using cached copy of init event in randomLandscapes module",
-  #                   capture_messages(sims <- spades(Copy(mySim), notOlderThan = Sys.time()))))
-  sims <- spades(Copy(mySim), notOlderThan = Sys.time())
-  dev.off()
-
-  expect_true(file.info(tmpfile)$size > 20000)
-  unlink(tmpfile)
-
-  landscapeMaps1 <- raster::dropLayer(sims$landscape, "Fires")
-  fireMap1 <- sims$landscape$Fires
-
-  # The cached version will be identical for both events (init and plot),
-  # but will not actually complete the plot, because plotting isn't cacheable
-  pdf(tmpfile)
-  mess1 <- capture_messages(sims <- spades(Copy(mySim)))
-  dev.off()
-
-  expect_true(file.info(tmpfile)$size < 10000)
-  unlink(tmpfile)
-
-  expect_true(any(grepl(pattern = "Using cached copy of init event in randomLandscapes module", mess1)))
-  landscapeMaps2 <- raster::dropLayer(sims$landscape, "Fires")
-  fireMap2 <- sims$landscape$Fires
-
-  # Test that cached part comes up identical in both (all maps but Fires),
-  #   but non-cached part are different (Fires should be different because stochastic)
-  expect_equal(landscapeMaps1, landscapeMaps2)
-  expect_false(isTRUE(suppressWarnings(all.equal(fireMap1, fireMap2))))
-
-  clearCache(sims)
-})
-
 test_that("test file-backed raster caching", {
   library(igraph)
   library(raster)
@@ -155,7 +12,8 @@ test_that("test file-backed raster caching", {
   nOT <- Sys.time()
 
   randomPolyToDisk <- function(tmpdir, tmpRasterfile) {
-    r <- randomPolygons(numTypes = 30)
+    r <- raster(extent(0, 10, 0, 10), vals=sample(1:30, size=100, replace = TRUE))
+    #r <- randomPolygons(numTypes = 30)
     writeRaster(r, tmpRasterfile, overwrite = TRUE)
     r <- raster(tmpRasterfile)
     r
@@ -213,7 +71,8 @@ test_that("test file-backed raster caching", {
 
   # Check that Caching of rasters saves them to tif file instead of rdata
   randomPolyToMemory <- function(tmpdir) {
-    r <- randomPolygons(numTypes = 30)
+    r <- raster(extent(0, 10, 0, 10), vals=sample(1:30, size=100, replace = TRUE))
+    #r <- randomPolygons(numTypes = 30)
     dataType(r) <- "INT1U"
     r
   }
@@ -227,7 +86,7 @@ test_that("test file-backed raster caching", {
 
   # Test that factors are saved correctly
   randomPolyToFactorInMemory <- function(tmpdir) {
-    r <- randomPolygons(numTypes = 30)
+    r <- raster(extent(0, 10, 0, 10), vals=sample(1:30, size=100, replace = TRUE))
     levels(r) <- data.frame(ID = 1:30, vals = sample(LETTERS[1:5], size = 30,replace = TRUE),
                             vals2 <- sample(1:7, size = 30, replace = TRUE))
     dataType(r) <- "INT1U"
@@ -235,13 +94,13 @@ test_that("test file-backed raster caching", {
   }
   bb <- Cache(randomPolyToFactorInMemory, tmpdir, cacheRepo = tmpdir)
   expect_true(dataType(bb) == "INT1U")
-  expect_true(is.factor(bb))
-  expect_true(is(levels(bb)[[1]], "data.frame"))
-  expect_true(NCOL(levels(bb)[[1]]) == 3)
-  expect_true(NROW(levels(bb)[[1]]) == 30)
+  expect_true(raster::is.factor(bb))
+  expect_true(is(raster::levels(bb)[[1]], "data.frame"))
+  expect_true(NCOL(raster::levels(bb)[[1]]) == 3)
+  expect_true(NROW(raster::levels(bb)[[1]]) == 30)
 
   randomPolyToFactorOnDisk <- function(tmpdir, tmpFile) {
-    r <- randomPolygons(numTypes = 30)
+    r <- raster(extent(0, 10, 0, 10), vals=sample(1:30, size=100, replace = TRUE))
     levels(r) <- data.frame(ID = 1:30, vals = sample(LETTERS[1:5], size = 30, replace = TRUE),
                             vals2 = sample(1:7, size = 30, replace = TRUE))
     r <- writeRaster(r, tmpFile, overwrite = TRUE, datatype = "INT1U")
@@ -262,10 +121,10 @@ test_that("test file-backed raster caching", {
   expect_true(dirname(filename(bb1)) == dirname(tf))
   expect_true(basename(filename(bb1)) == basename(tf))
   expect_true(dataType(bb) == "INT1U")
-  expect_true(is.factor(bb))
-  expect_true(is(levels(bb)[[1]], "data.frame"))
-  expect_true(NCOL(levels(bb)[[1]]) == 3)
-  expect_true(NROW(levels(bb)[[1]]) == 30)
+  expect_true(raster::is.factor(bb))
+  expect_true(is(raster::levels(bb)[[1]], "data.frame"))
+  expect_true(NCOL(raster::levels(bb)[[1]]) == 3)
+  expect_true(NROW(raster::levels(bb)[[1]]) == 30)
 
   clearCache(tmpdir)
 })
@@ -290,48 +149,6 @@ test_that("test date-based cache removal", {
   b <- clearCache(tmpdir, before = Sys.Date() + 1)
   expect_identical(b, a1)
 
-  # Example of changing parameter values
-
-  clearCache(tmpdir)
-
-  mySim <- simInit(
-    times = list(start = 0.0, end = 1.0, timeunit = "year"),
-    params = list(
-      .globals = list(stackName = "landscape", burnStats = "nPixelsBurned"),
-      # Turn off interactive plotting
-      fireSpread = list(.plotInitialTime = NA),
-      caribouMovement = list(.plotInitialTime = NA),
-      randomLandscapes = list(.plotInitialTime = NA, .useCache = "init")
-    ),
-    modules = list("randomLandscapes", "fireSpread", "caribouMovement"),
-    paths = list(modulePath = system.file("sampleModules", package = "SpaDES"),
-                 outputPath = tmpdir,
-                 cachePath = tmpdir),
-    # Save final state of landscape and caribou
-    outputs = data.frame(objectName = c("landscape", "caribou"),
-                         stringsAsFactors = FALSE)
-  )
-  clearCache(mySim)
-  sims <- spades(Copy(mySim), notOlderThan = Sys.time())
-  simsCacheID <- unlist(gsub(attr(sims, "tags"), pattern = "cacheId:", replacement = ""))
-  ranNums <- Cache(runif, 4, cacheRepo = cachePath(mySim), userTags = "objectName:a")
-  # access it again, but "later"
-  Sys.sleep(1)
-  sims <- spades(Copy(mySim)) # i.e., this is a "read" operation, does not create a new artifact
-  wholeCache <- showCache(mySim)
-  expect_true(length(unique(wholeCache, by = "artifact")$artifact) == 2)
-  expect_true(sum(wholeCache$tagKey == "accessed") == 3)
-  # keep only items accessed "recently"
-  onlyRecentlyAccessed <- showCache(mySim,
-                                    userTags = max(wholeCache[tagKey == "accessed"]$tagValue))
-  # inverse join with 2 data.tables ... using: a[!b]
-      # i.e., return all of wholeCache that was not recently accessed
-  toRemove <- unique(wholeCache[!onlyRecentlyAccessed], by = "artifact")$artifact
-  clearCache(mySim, toRemove) # remove ones not recently accessed
-  expect_true(length(unique(showCache(mySim), by = "artifact")$artifact) == 1)
-  expect_true(sum(showCache(mySim)$tagKey == "accessed") == 2)
-  # make sure it is the sims one
-  expect_true(length(unique(showCache(mySim, simsCacheID)$artifact)) == 1)
 })
 
 test_that("test keepCach", {
