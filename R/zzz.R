5f649a89ad95dcd5a01b1ca535cc21bb6adeb370
Eliot McIntire <eliotmcintire@gmail.com>
2018-03-02 00:50:58 -0800

    memoising not so fast

--- R/zzz.R
+++ R/zzz.R
@@ -8,7 +8,7 @@
   opts.reproducible <- list( # nolint
     reproducible.cachePath = file.path(.reproducibleTempDir, "cache"),
     #reproducible.digestPathContent = !reproducible.quick,
-    reproducible.useMemoise = TRUE,
+    reproducible.useMemoise = FALSE,
     reproducible.quick = reproducible.quick
   )
   toset <- !(names(opts.reproducible) %in% names(opts))
