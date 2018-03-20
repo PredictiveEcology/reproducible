af544ee70d3ae8b0bc938f76ec36490e91ebc897
Eliot McIntire <eliotmcintire@gmail.com>
2018-03-20 13:20:12 -0700

    test-examples -- a conditional write to file for debugging

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -1,9 +1,15 @@
 test_that("all exported functions have examples", {
   fns <- ls("package:reproducible")
   omit <- which(fns == "cache") ## cache is deprecated, so omit it
+  if (grepl("VIC-", Sys.info()["nodename"])) { # for debugging only
+    cat("###############\n", file = "C:/Eliot/tmp/examples.txt", append = FALSE)
   sapply(fns[-omit], function(x) {
-    expect_warning(example(x, package = "reproducible", character.only = TRUE,
-                           echo = FALSE), NA)
+    warn <- capture_warning(example(x, package = "reproducible", character.only = TRUE,
+                           echo = FALSE))
+    if (grepl("VIC-", Sys.info()["nodename"])) { # for debugging only
+      cat(paste(x, " -- ", warn, "\n"), file = "C:/Eliot/tmp/examples.txt", append = TRUE)
+    }
+    #expect_true(length(warn)>0)
   })
 })
 
