d502437857e41114b9c28e3d5fb0d6144b4221ca
Eliot McIntire <eliotmcintire@gmail.com>
2018-07-08 01:08:13 -0700

    showCache/keepCache/clearCache -- large grep & messaging & speed

--- R/cache-tools.R
+++ R/cache-tools.R
@@ -122,8 +122,9 @@ setMethod(
     objsDT <- do.call(showCache, args = args)
 
     if (interactive()) {
-      rdaFiles <- file.path(x, "gallery", paste0(unique(objsDT$artifact), ".rda"))
-      cacheSize <- sum(file.size(rdaFiles))
+      cacheSize <- sum(as.numeric(objsDT[tagKey=="object.size"]$tagValue))/4
+      #rdaFiles <- file.path(x, "gallery", paste0(unique(objsDT$artifact), ".rda"))
+      #cacheSize <- sum(file.size(rdaFiles))
     }
 
     if (NROW(objsDT)) {
@@ -149,7 +150,7 @@ setMethod(
         class(cacheSize) <- "object_size"
         formattedCacheSize <- format(cacheSize, "auto")
         if (cacheSize > 1e7) {
-          message("Your current cache size is ", formattedCacheSize, ".\n",
+          message("Your size of your selected objects is ", formattedCacheSize, ".\n",
                   " Are you sure you would like to delete it all? Y or N")
           rl <- readline()
           if (!identical(toupper(rl), "Y")) {
@@ -216,18 +217,27 @@ setMethod(
                                                 (tagValue >= after)][!duplicated(artifact)]
       objsDT <- objsDT[artifact %in% objsDT3$artifact]
       if (length(userTags) > 0) {
-        for (ut in userTags) {
-          objsDT2 <- objsDT[
-            grepl(tagValue, pattern = ut) |
-              grepl(tagKey, pattern = ut) |
-              grepl(artifact, pattern = ut)]
-          setkeyv(objsDT2, "artifact")
-          shortDT <- unique(objsDT2, by = "artifact")[, artifact]
-          objsDT <- if (NROW(shortDT)) objsDT[shortDT] else objsDT[0] # merge each userTags
-        }
+        objsDT <- objsDT[artifact %in% userTags | tagKey %in% userTags | tagValue %in% userTags]
+        # for (ut in userTags) {
+        #   #objsDT$artifact %in% ut
+        #   objsDT2 <- objsDT[
+        #     grepl(tagValue, pattern = ut) |
+        #       grepl(tagKey, pattern = ut) |
+        #       grepl(artifact, pattern = ut)]
+        #   setkeyv(objsDT2, "artifact")
+        #   shortDT <- unique(objsDT2, by = "artifact")[, artifact]
+        #   objsDT <- if (NROW(shortDT)) objsDT[shortDT] else objsDT[0] # merge each userTags
+        # }
       }
     }
-    .messageCacheSize(x, artifacts = unique(objsDT$artifact))
+    verboseMessaging <- TRUE
+    if (!is.null(list(...)$verboseMessaging)) {
+      if (!isTRUE(list(...)$verboseMessaging)) {
+        verboseMessaging <- FALSE
+      }
+    }
+    if (verboseMessaging)
+      .messageCacheSize(x, artifacts = unique(objsDT$artifact))
     objsDT
 })
 
@@ -258,8 +268,8 @@ setMethod(
     eliminate <- unique(objsDTAll$artifact[!(objsDTAll$artifact %in% keep)])
 
     if (length(eliminate)) {
-      eliminate <- paste(eliminate, collapse = "|") ## TODO: remove
-      clearCache(x, eliminate)
+      #eliminate <- paste(eliminate, collapse = "|") ## TODO: remove
+      clearCache(x, eliminate, verboseMessaging = FALSE)
     }
     return(objsDT)
 })
@@ -344,14 +354,16 @@ setMethod(
 
 #' @keywords internal
 .messageCacheSize <- function(x, artifacts = NULL) {
-  fsTotal <- sum(file.size(dir(x, full.names = TRUE, recursive = TRUE)))
+  a <- showLocalRepo2(x);
+  b <- a[startsWith(a$tag, "object.size"),]
+  fsTotal <- sum(as.numeric(unlist(lapply(strsplit(b$tag, split = ":"), function(x) x[[2]])))) / 4
+  fsTotalRasters <- sum(file.size(dir(file.path(x, "rasters"), full.names = TRUE, recursive = TRUE)))
+  fsTotal <- fsTotal + fsTotalRasters
   class(fsTotal) <- "object_size"
   preMessage1 <- "  Total (including Rasters): "
 
-  allFiles <- dir(file.path(x, "gallery"), full.names = TRUE)
-  matchingIDs <- which(basename(allFiles) %in% paste0(unique(artifacts), ".rda"))
-  matchingFiles <- allFiles[matchingIDs]
-  fs <- sum(file.size(matchingFiles))
+  b <- a[a$artifact %in% artifacts & startsWith(a$tag, "object.size"),]
+  fs <- sum(as.numeric(unlist(lapply(strsplit(b$tag, split = ":"), function(x) x[[2]])))) / 4
 
   class(fs) <- "object_size"
   preMessage <- "  Selected objects (not including Rasters): "
