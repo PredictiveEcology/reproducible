95ff08ce9928b8460df84480078fe49523471990
CeresBarros <cbarros@mail.ubc.ca>
2018-07-20 14:39:02 -0700

    bugfix: overwrite was duplicated in .prepareFileBackedRaster call from .prepareOutput

--- R/cache-helpers.R
+++ R/cache-helpers.R
@@ -261,7 +261,7 @@ setMethod(
   definition = function(object, cacheRepo, ...) {
     # with this call to .prepareFileBackedRaster, it is from the same function call as a previous time
     #  overwrite is ok
-    .prepareFileBackedRaster(object, repoDir = cacheRepo, overwrite = TRUE, ...)
+    .prepareFileBackedRaster(object, repoDir = cacheRepo, ...)
 })
 
 #' @export
