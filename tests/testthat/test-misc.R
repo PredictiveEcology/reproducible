96bea4915375c210ab1991fbc6db93b35cfa15e0
Eliot McIntire <eliotmcintire@gmail.com>
2018-09-08 20:57:07 -0700

    test-misc.R -- new unitTests for untested fns
    searchFull
    convertPaths
    convertRasterPaths

---
+++ tests/testthat/test-misc.R
@@ -0,0 +1,42 @@
+test_that("test miscellaneous fns", {
+  testInitOut <- testInit("raster", tmpFileExt = c(".tif", ".grd"))
+  on.exit({
+    testOnExit(testInitOut)
+  }, add = TRUE)
+
+  expect_is(searchFullEx(), "list")
+  expect_true(length(searchFullEx()) > length(search()))
+  expect_true(length(searchFullEx()) == (3 + length(search())))
+
+  expect_true(all(unlist(lapply(searchFull(simplify = FALSE), is.environment))))
+  expect_true(all(is.character(unlist(lapply(searchFull(simplify = FALSE), attributes)))))
+
+  # objectSize
+  a <- 1
+  b <- tempfile()
+  saveRDS(a, b)
+  expect_is(objSize(asPath(b)), "numeric")
+  expect_is(objSize(asPath(b), quick = TRUE), "object_size")
+
+  # convertRasterPaths
+  filenames <- normalizePath(c("/home/user1/Documents/file.txt", "/Users/user1/Documents/file.txt"),
+                              winslash = "/", mustWork = FALSE)
+  oldPaths <- dirname(filenames)
+  newPaths <- normalizePath(c("/home/user2/Desktop", "/Users/user2/Desktop"),
+                            winslash = "/", mustWork = FALSE)
+  expect_true(grepl(newPaths[1], convertPaths(filenames, oldPaths, newPaths)[1]))
+
+  r1 <- raster::raster(system.file("external/test.grd", package = "raster"))
+  r2 <- raster::raster(system.file("external/rlogo.grd", package = "raster"))
+  rasters <- list(r1, r2)
+  oldPaths <- system.file("external", package = "raster")
+  newPaths <- file.path("~/rasters")
+  rasters <- convertRasterPaths(rasters, oldPaths, newPaths)
+  expect_true(identical(unlist(lapply(rasters, raster::filename)),
+                        path.expand(file.path(newPaths, basename(unlist(lapply(list(r1,r2), raster::filename)))))))
+
+
+  r1 <- writeRaster(r1, tmpfile[1], overwrite = TRUE)
+  r3 <- convertRasterPaths(tmpfile[1], dirname(tmpfile[1]), newPaths)
+  d
+})
