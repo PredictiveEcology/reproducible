72da5b8736066ce5579535c21d153f650c72981d
Eliot McIntire <eliotmcintire@gmail.com>
2018-10-19 14:40:13 -0700

    prepInputs -- missing overwrite = overwrite in postProcess

--- R/prepInputs.R
+++ R/prepInputs.R
@@ -314,6 +314,7 @@ prepInputs <- function(targetFile = NULL, url = NULL, archive = NULL, alsoExtrac
   if (!(all(is.null(out$dots$studyArea), is.null(out$dots$rasterToMatch)))) {
     message("Running postProcess")
     x <- Cache(do.call, postProcess, append(list(x = x, filename1 = out$targetFilePath,
+                                                 overwrite = overwrite,
                                                  destinationPath = out$destinationPath), out$dots),
                useCache = useCache)
   }
