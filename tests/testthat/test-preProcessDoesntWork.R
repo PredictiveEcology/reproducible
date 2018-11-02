36de0632fd1ca3295dd1efafff9998a7beaedaf5
Tati Micheletti <tati.micheletti@gmail.com>
2018-11-02 10:27:03 -0700

    fixes to test-preProcessDoesntWork

--- tests/testthat/test-preProcessDoesntWork.R
+++ tests/testthat/test-preProcessDoesntWork.R
@@ -1,21 +1,32 @@
 test_that("preProcess fails if user provides a non .zip/.tar as archive", {
+  testInitOut <- testInit("raster", needGoogle = TRUE)
+  on.exit({
+    testOnExit(testInitOut)
+  }, add = TRUE)
   pre <- reproducible::preProcess(url = "https://drive.google.com/open?id=1cGFQlfe719nrPiN8HepmgB8vy7NRL8dR",
-                                  destinationPath = raster::tmpDir())
+                                  destinationPath = tmpdir)
   testthat::expect_is(object = pre, class = "list")
   testthat::expect_error(ras <- reproducible::preProcess(archive = pre$targetFilePath))
 })
 
 test_that("preProcess fails if user provides non-existing file", {
-
+  testInitOut <- testInit("raster", needGoogle = TRUE)
+  on.exit({
+    testOnExit(testInitOut)
+  }, add = TRUE)
   pre <- reproducible::preProcess(url = "https://drive.google.com/open?id=1cGFQlfe719nrPiN8HepmgB8vy7NRL8dR",
-                                  destinationPath = raster::tmpDir())
+                                  destinationPath = tmpdir)
   testthat::expect_is(object = pre, class = "list")
   testthat::expect_error(ras <- reproducible::preProcess(archive = "fileDoesNotExist.zip"))
 })
 
 test_that("preProcess fails if user provides a directory as a targetFile", {
+  testInitOut <- testInit("raster", needGoogle = TRUE)
+  on.exit({
+    testOnExit(testInitOut)
+  }, add = TRUE)
   pre <- reproducible::preProcess(url = "https://drive.google.com/open?id=1cGFQlfe719nrPiN8HepmgB8vy7NRL8dR",
-                                  destinationPath = raster::tmpDir())
+                                  destinationPath = tmpdir)
   testthat::expect_is(object = pre, class = "list")
-  testthat::expect_error(ras <- reproducible::preProcess(targetFile = raster::tmpDir()))
+  testthat::expect_error(ras <- reproducible::preProcess(targetFile = tmpdir))
 })
