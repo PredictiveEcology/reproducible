d274352d887258d55a3a4d5bc5aea65a0ca8978f
Eliot McIntire <eliotmcintire@gmail.com>
2018-11-06 10:42:18 -0800

    nextNumericModule bugfix

--- R/cache-helpers.R
+++ R/cache-helpers.R
@@ -1099,7 +1099,7 @@ nextNumericName <- function(string) {
     splits <- strsplit(allSimilarFilesInDirSansExt[alreadyHasNumeric], split = "_")
     highestNumber <- max(unlist(lapply(splits, function(split) as.numeric(tail(split,1)))),
                          na.rm = TRUE)
-    preNumeric <- sapply(splits, function(spl) spl[-length(spl)])
+    preNumeric <- lapply(splits, function(spl) paste(spl[-length(spl)], collapse = "_"))
 
     out <- paste0(preNumeric, "_", highestNumber + 1) # keep rndstr in here, so that both streams keep same rnd number state
   } else {
