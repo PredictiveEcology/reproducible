920074940686554c366a29fa34732bbc768289d2
Eliot McIntire <eliotmcintire@gmail.com>
2019-09-10 09:02:38 -0700

    clean up for R CMD check

--- R/objectSize.R
+++ R/objectSize.R
@@ -3,15 +3,16 @@
 #'
 #' This has methods for various types of things that may not correctly report
 #' their object.size using \code{object\.size}. Also, for lists and environments,
-#' it will return the object.size separately for each element.
+#' it will return the object.size separately for each element. These are estimates
+#' only, and could be inaccurate. Alternative, similar functions include
+#' \code{object.size} and \code{pryr::object_size}. See Details for the special case of
+#' functions and their enclosing environments.
 #'
 #' @param x An object
 #' @param quick Logical. Only some methods use this. e.g.,
 #'              \code{Path} class objects. In which case, \code{file.size} will be
 #'              used instead of \code{object.size}.
 #' @param .prevEnvirs For internal account keeping to identify and prevent duplicate counting
-#' @param ... Other arguments passed to methods, e.g., \code{objSize.function}
-#'   has \code{enclosingEnvs}
 #'
 #' @export
 #' @rdname objSize
@@ -24,27 +25,39 @@
 #' objSize(a) # all the elements in the environment
 #' object.size(a) # different - only measuring the environment as an object
 #'
+#' object.size(prepInputs) # only the function, without its enclosing environment
+#' objSize(prepInputs)     # the function, plus its enclosing environment
+#'
+#' # Size of all packages; includes their imported functions
 #' a <- objSizeSession(1)
 #' print(a, units = "auto")
-objSize <- function(x, quick, .prevEnvirs, ...) {
+#'
+#' os1 <- object.size(as.environment("package:reproducible"))
+#' os2 <- objSize(as.environment("package:reproducible"))
+#' (os1) # very small -- just the environment container
+#' sum(unlist(os2)) # around 31 MB, with all functions, objects
+#'                  # and imported functions
+objSize <- function(x, quick, enclosingEnvs, .prevEnvirs) {
   UseMethod("objSize", x)
 }
 
 #' @export
 #' @rdname objSize
 objSize.list <- function(x, quick = getOption("reproducible.quick", FALSE),
-                         .prevEnvirs = list(), ...) {
+                        enclosingEnvs = TRUE, .prevEnvirs = list()) {
   TandC <- grepl(".__[TC]__", names(x))
   if (sum(TandC) > 0)
     x <- x[!TandC]
   osList <- lapply(x, function(y) {
     if (!is.function(y)) {
-      os <- objSize(y, quick = quick, .prevEnvirs = .prevEnvirs, ...)
+      os <- objSize(y, quick = quick, enclosingEnvs = enclosingEnvs,
+                    .prevEnvirs = .prevEnvirs)
     } else {
       doneAlready <- lapply(.prevEnvirs, function(pe) identical(pe, environment(y)))
       .prevEnvirs <<- unique(append(.prevEnvirs, environment(y)))
       if (!any(unlist(doneAlready))) {
-        os <- objSize(y, quick = quick, .prevEnvirs = .prevEnvirs, ...)
+        os <- objSize(y, quick = quick, enclosingEnvs =enclosingEnvs,
+                      .prevEnvirs = .prevEnvirs)
       } else {
         os <- NULL
       }
@@ -60,10 +73,11 @@ objSize.list <- function(x, quick = getOption("reproducible.quick", FALSE),
 #' @rdname objSize
 #' @importFrom utils object.size
 objSize.environment <- function(x, quick = getOption("reproducible.quick", FALSE),
-                                .prevEnvirs = list(), ...) {
+                                enclosingEnvs = TRUE, .prevEnvirs = list()) {
   xName <- deparse(substitute(x))
   print(format(x))
-  os <- objSize(as.list(x, all.names = TRUE), .prevEnvirs = .prevEnvirs, ...)
+  os <- objSize(as.list(x, all.names = TRUE), enclosingEnvs = enclosingEnvs,
+                .prevEnvirs = .prevEnvirs)
   if (length(os) > 0)
     names(os) <- paste0(xName, "$", names(os))
   osCur <- list(object.size(x))
@@ -74,13 +88,15 @@ objSize.environment <- function(x, quick = getOption("reproducible.quick", FALSE
 
 #' @export
 #' @rdname objSize
-objSize.default <- function(x, quick = getOption("reproducible.quick", FALSE), ...) {
+objSize.default <- function(x, quick = getOption("reproducible.quick", FALSE),
+                            enclosingEnvs = TRUE, .prevEnvirs = list()) {
   object.size(x)
 }
 
 #' @export
 #' @rdname objSize
-objSize.Path <- function(x, quick = getOption("reproducible.quick", FALSE), ...) {
+objSize.Path <- function(x, quick = getOption("reproducible.quick", FALSE),
+                         enclosingEnvs = TRUE, .prevEnvirs = list()) {
   if (quick) {
     object.size(x)
   } else {
@@ -98,7 +114,7 @@ objSize.Path <- function(x, quick = getOption("reproducible.quick", FALSE), ...)
 #' However, if the enclosing environment is the \code{.GlobalEnv}, it will
 #' not be included even though \code{enclosingEnvs = TRUE}.
 objSize.function <- function(x, quick = getOption("reproducible.quick", FALSE),
-                             enclosingEnvs = TRUE, .prevEnvirs = list(), ...) {
+                             enclosingEnvs = TRUE, .prevEnvirs = list()) {
   varName <- deparse(substitute(x))
   if (isTRUE(enclosingEnvs) && (!identical(.GlobalEnv, environment(x)))) {
     if (is.primitive(x)) {
@@ -124,7 +140,7 @@ objSize.function <- function(x, quick = getOption("reproducible.quick", FALSE),
 #' Because of the difficulties in calculating the object size of \code{base}
 #' and \code{methods} packages and \code{Autoloads}, these are
 #' omitted.
-objSizeSession <- function(sumLevel = Inf, .prevEnvirs = list(), ...) {
+objSizeSession <- function(sumLevel = Inf, enclosingEnvs = TRUE, .prevEnvirs = list()) {
   srch <- search()
   srch <- setdiff(srch, c("package:base", "package:methods", "Autoloads"))
   names(srch) <- srch
@@ -132,7 +148,8 @@ objSizeSession <- function(sumLevel = Inf, .prevEnvirs = list(), ...) {
     doneAlready <- lapply(.prevEnvirs, function(pe) identical(pe, as.environment(x)))
     .prevEnvirs <<- unique(append(.prevEnvirs, as.environment(x)))
     out <- if (!any(unlist(doneAlready))) {
-      try(objSize(as.environment(x), .prevEnvirs = .prevEnvirs, ...))
+      try(objSize(as.environment(x), enclosingEnvs = enclosingEnvs,
+                  .prevEnvirs = .prevEnvirs))
     } else {
       NULL
     }
