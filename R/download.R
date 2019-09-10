4b0efeb57ae9cf93802f1462e08a2b35aedae4c6
Alex Chubaty <alex.chubaty@gmail.com>
2019-09-10 11:02:06 -0600

    fix drive re-auth issue

--- R/download.R
+++ R/download.R
@@ -442,7 +442,7 @@ assessGoogle <- function(url, archive = NULL, targetFile = NULL,
     on.exit(options(opts))
   }
 
-  if (!is.null(googledrive::drive_token()))
+  if (is.null(googledrive::drive_token()))
     googledrive::drive_auth() ## needed for use on e.g., rstudio-server
 
   if (is.null(archive)) {
