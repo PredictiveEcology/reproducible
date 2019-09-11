8a44ae0a92810ccf183364d1e1ab291ff5662c98
Alex Chubaty <alex.chubaty@gmail.com>
2019-09-11 11:41:20 -0600

    objSize docs cleanup

--- R/objectSize.R
+++ R/objectSize.R
@@ -1,12 +1,11 @@
-
-#' Recursive object.size
+#' Recursive \code{object.size}
 #'
 #' This has methods for various types of things that may not correctly report
-#' their object.size using \code{object\.size}. Also, for lists and environments,
-#' it will return the object.size separately for each element. These are estimates
-#' only, and could be inaccurate. Alternative, similar functions include
-#' \code{object.size} and \code{pryr::object_size}. See Details for the special case of
-#' functions and their enclosing environments.
+#' their object size using \code{object.size}.
+#' Also, for lists and environments, it will return the object size separately for each element.
+#' These are estimates only, and could be inaccurate.
+#' Alternative, similar functions include \code{object.size} and \code{pryr::object_size}.
+#' See Details for the special case of functions and their enclosing environments.
 #'
 #' @param x An object
 #' @param quick Logical. Only some methods use this. e.g.,
@@ -16,22 +15,24 @@
 #'
 #' @export
 #' @rdname objSize
-#' @keywords internal
-#' @examples
-#' a <- new.env()
-#' a$b <- 1:10
-#' a$d <- 1:10
 #'
-#' objSize(a) # all the elements in the environment
-#' object.size(a) # different - only measuring the environment as an object
+#' @examples
+#' library(utils)
+#'
+#' foo <- new.env()
+#' foo$b <- 1:10
+#' foo$d <- 1:10
+#'
+#' objSize(foo) # all the elements in the environment
+#' object.size(foo) # different - only measuring the environment as an object
 #'
 #' object.size(prepInputs) # only the function, without its enclosing environment
 #' objSize(prepInputs)     # the function, plus its enclosing environment
 #'
 #' # Size of all packages; includes their imported functions
 #' \dontrun{
-#'   a <- objSizeSession(1)
-#'   print(a, units = "auto")
+#'   bar <- objSizeSession(1)
+#'   print(bar, units = "auto")
 #' }
 #'
 #' os1 <- object.size(as.environment("package:reproducible"))
@@ -39,6 +40,7 @@
 #' (os1) # very small -- just the environment container
 #' sum(unlist(os2)) # around 31 MB, with all functions, objects
 #'                  # and imported functions
+#'
 objSize <- function(x, quick, enclosingEnvs, .prevEnvirs) {
   UseMethod("objSize", x)
 }
@@ -52,14 +54,12 @@ objSize.list <- function(x, quick = getOption("reproducible.quick", FALSE),
     x <- x[!TandC]
   osList <- lapply(x, function(y) {
     if (!is.function(y)) {
-      os <- objSize(y, quick = quick, enclosingEnvs = enclosingEnvs,
-                    .prevEnvirs = .prevEnvirs)
+      os <- objSize(y, quick = quick, enclosingEnvs = enclosingEnvs, .prevEnvirs = .prevEnvirs)
     } else {
       doneAlready <- lapply(.prevEnvirs, function(pe) identical(pe, environment(y)))
       .prevEnvirs <<- unique(append(.prevEnvirs, environment(y)))
       if (!any(unlist(doneAlready))) {
-        os <- objSize(y, quick = quick, enclosingEnvs =enclosingEnvs,
-                      .prevEnvirs = .prevEnvirs)
+        os <- objSize(y, quick = quick, enclosingEnvs = enclosingEnvs, .prevEnvirs = .prevEnvirs)
       } else {
         os <- NULL
       }
@@ -72,8 +72,8 @@ objSize.list <- function(x, quick = getOption("reproducible.quick", FALSE),
 }
 
 #' @export
-#' @rdname objSize
 #' @importFrom utils object.size
+#' @rdname objSize
 objSize.environment <- function(x, quick = getOption("reproducible.quick", FALSE),
                                 enclosingEnvs = TRUE, .prevEnvirs = list()) {
   xName <- deparse(substitute(x))
@@ -89,6 +89,7 @@ objSize.environment <- function(x, quick = getOption("reproducible.quick", FALSE
 }
 
 #' @export
+#' @importFrom utils object.size
 #' @rdname objSize
 objSize.default <- function(x, quick = getOption("reproducible.quick", FALSE),
                             enclosingEnvs = TRUE, .prevEnvirs = list()) {
@@ -96,6 +97,7 @@ objSize.default <- function(x, quick = getOption("reproducible.quick", FALSE),
 }
 
 #' @export
+#' @importFrom utils object.size
 #' @rdname objSize
 objSize.Path <- function(x, quick = getOption("reproducible.quick", FALSE),
                          enclosingEnvs = TRUE, .prevEnvirs = list()) {
@@ -106,8 +108,6 @@ objSize.Path <- function(x, quick = getOption("reproducible.quick", FALSE),
   }
 }
 
-#' @export
-#' @rdname objSize
 #' @details
 #' For functions, a user can include the enclosing environment as described
 #' \url{https://www.r-bloggers.com/using-closures-as-objects-in-r/} and
@@ -115,6 +115,9 @@ objSize.Path <- function(x, quick = getOption("reproducible.quick", FALSE),
 #' It is not entirely clear which estimate is better.
 #' However, if the enclosing environment is the \code{.GlobalEnv}, it will
 #' not be included even though \code{enclosingEnvs = TRUE}.
+#'
+#' @export
+#' @rdname objSize
 objSize.function <- function(x, quick = getOption("reproducible.quick", FALSE),
                              enclosingEnvs = TRUE, .prevEnvirs = list()) {
   varName <- deparse(substitute(x))
@@ -129,19 +132,19 @@ objSize.function <- function(x, quick = getOption("reproducible.quick", FALSE),
     os <- object.size(x)
   }
   return(os)
-
 }
 
-#' @rdname objSize
 #' @param sumLevel Numeric, indicating at which depth in the list of objects should the
 #'   object sizes be summed (summarized). Default is \code{Inf}, meaning no sums. Currently,
 #'   the only option other than Inf is 1: \code{objSizeSession(1)},
 #'   which gives the size of each package.
-#' @export
+#'
 #' @details \code{objSizeSession} will give the size of the whole session, including loaded packages.
 #' Because of the difficulties in calculating the object size of \code{base}
-#' and \code{methods} packages and \code{Autoloads}, these are
-#' omitted.
+#' and \code{methods} packages and \code{Autoloads}, these are omitted.
+#'
+#' @export
+#' @rdname objSize
 objSizeSession <- function(sumLevel = Inf, enclosingEnvs = TRUE, .prevEnvirs = list()) {
   srch <- search()
   srch <- setdiff(srch, c("package:base", "package:methods", "Autoloads"))
@@ -173,4 +176,3 @@ objSizeSession <- function(sumLevel = Inf, enclosingEnvs = TRUE, .prevEnvirs = l
 
   return(os)
 }
-
