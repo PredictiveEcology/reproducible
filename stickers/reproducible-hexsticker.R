536920fef2cfc418fd79f66ff1b72e399306982c
Alex Chubaty <alex.chubaty@gmail.com>
2019-10-10 10:28:05 -0600

    sticker code fix
    use actual url instead of transient google link
    fix path to png

--- stickers/reproducible-hexsticker.R
+++ stickers/reproducible-hexsticker.R
@@ -9,13 +9,11 @@ font_add_google("Bree Serif", "bree")
 ## Automatically use showtext to render text for future devices
 showtext_auto()
 
-imgurl <- "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjmzIeE_5DlAhXPIDQIHRWdDk0QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F2651766%2Farrows_hexagon_loop_reload_reverse_synchronize_icon&psig=AOvVaw0ZHyd4NP6-LCL0iNRaxbk8&ust=1570772782869727"
-
-dir.create("inst/figures")
+imgurl <- "https://cdn2.iconfinder.com/data/icons/synchronize-1/24/synchronize-hexagon-1-512.png"
 
 sticker(imgurl, package = "reproducible",
         h_color = "#006dff", h_fill = "#cccccc",
         p_color = "#006dff", p_family = "bree", p_size = 18, p_x = 0.85, p_y = 1.3,
         s_x = 1.1, s_y = 1, s_width = 0.8, s_height = 0.8,
         url = "http://reproducible.predictiveecology.org", u_color = "#006dff", u_size = 3.8,
-        filename = "~/GitHub/PredictiveEcology/reproducible/inst/figures/hexsticker.png", spotlight = FALSE)
+        filename = "stickers/hexsticker.png", spotlight = FALSE)
