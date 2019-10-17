626c304d8932c523482169a43c2b3252db801a07
Alex Chubaty <alex.chubaty@gmail.com>
2019-10-16 21:49:26 -0600

    remove url from hexsticker

--- stickers/reproducible-hexsticker.R
+++ stickers/reproducible-hexsticker.R
@@ -16,5 +16,5 @@ sticker(imgurl, package = "reproducible",
         h_color = "#006dff", h_fill = "#cccccc",
         p_color = "#006dff", p_family = "bree", p_size = 18, p_x = 0.85, p_y = 1.3,
         s_x = 1.05, s_y = 1, s_width = 0.8, s_height = 0.8,
-        url = "http://reproducible.predictiveecology.org", u_color = "#006dff", u_size = 3.8,
+        #url = "http://reproducible.predictiveecology.org", u_color = "#006dff", u_size = 3.8,
         filename = "stickers/hexsticker.png", spotlight = FALSE)
