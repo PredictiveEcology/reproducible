8f32af1661acdc0b7f49b0349c2594027d02d85a
Eliot McIntire <eliotmcintire@gmail.com>
2019-11-13 14:51:29 -0800

    bugfix for skip_if_no_token

--- tests/testthat/helper-allEqual.R
+++ tests/testthat/helper-allEqual.R
@@ -5,7 +5,7 @@ all.equalWONewCache <- function(a, b) {
 }
 
 skip_if_no_token <- function() {
-  testthat::skip_if_not(drive_has_token(), "No Drive token")
+  testthat::skip_if_not(googledrive::drive_has_token(), "No Drive token")
 }
 
 # puts tmpdir, tmpCache, tmpfile (can be vectorized with length >1 tmpFileExt),
