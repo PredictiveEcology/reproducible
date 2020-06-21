4f85c52da7ac1e82df887eabe306639fb715eebb
Eliot McIntire <eliotmci@mail.ubc.ca>
2020-06-20 23:42:46 -0700

    reexports

---
+++ R/reexports.R
@@ -0,0 +1,11 @@
+#' @importFrom Require Require
+#' @export
+Require::Require
+
+#' @importFrom Require pkgDep
+#' @export
+Require::pkgDep
+
+#' @importFrom Require checkPath
+#' @export
+Require::checkPath
