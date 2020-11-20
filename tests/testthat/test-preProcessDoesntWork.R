d8316821963b015a00f386b5f9a354c4d838e4e2
Eliot <eliotmci@mail.ubc.ca>
2020-11-20 12:19:49 -0800

    Add interactive for fail

--- tests/testthat/test-preProcessDoesntWork.R
+++ tests/testthat/test-preProcessDoesntWork.R
@@ -1,15 +1,62 @@
 test_that("preProcess fails if user provides non-existing file", {
   skip_on_cran()
-  testInitOut <- testInit("raster", needGoogle = FALSE)
+  testInitOut <- testInit("raster", needGoogle = FALSE, opts = list(reproducible.inputPaths = NULL))
   on.exit({
     testOnExit(testInitOut)
   }, add = TRUE)
-  errMsg <- testthat::capture_error <- capture_error(reproducible::preProcess(
+  testthat::with_mock(
+    `reproducible:::isInteractive` = function() {FALSE},
+    {
+    errMsg <- testthat::capture_error(reproducible::preProcess(
+      url = "https://github.com/tati-micheletti/host/raw/master/data/rasterTest",
+      destinationPath = tmpdir
+    ))
+    })
+  expect_true(grepl("try manual", errMsg))
+  expect_true(grepl("appendChecksumsTable", errMsg))
+
+  optsOrig <- options('reproducible.interactiveOnDownloadFail' = FALSE)
+  errMsg <- testthat::capture_error(reproducible::preProcess(
     url = "https://github.com/tati-micheletti/host/raw/master/data/rasterTest",
     destinationPath = tmpdir
   ))
   expect_true(grepl("try manual", errMsg))
   expect_true(grepl("appendChecksumsTable", errMsg))
+  options(optsOrig)
+
+  testthat::with_mock(
+    `reproducible:::isInteractive` = function() {TRUE},
+    `reproducible:::.readline` = function(prompt) {"n"},
+    {
+      mess <- testthat::capture_messages(
+        errMsg <- testthat::capture_error(reproducible::preProcess(
+        url = "https://github.com/tati-micheletti/host/raw/master/data/rasterTest",
+        destinationPath = tmpdir
+      )))
+    })
+  expect_true(sum(grepl("manual download", mess)) == 1)
+  expect_true(sum(grepl("To prevent", mess)) == 1)
+
+  testthat::with_mock(
+    `reproducible:::isInteractive` = function() {TRUE},
+    `reproducible:::.readline` = function(prompt) {
+      file.create(file.path(tmpdir, "rasterTest.zip"))
+      "y"
+      },
+    {
+      mess <- testthat::capture_messages(
+        errMsg <- testthat::capture_error(reproducible::preProcess(
+          url = "https://github.com/tati-micheletti/host/raw/master/data/rasterTest",
+          destinationPath = tmpdir
+        )))
+    })
+  expect_true(sum(grepl("manual download", mess)) == 1)
+  expect_true(sum(grepl("To prevent", mess)) == 1)
+  expect_true(file.exists(file.path(tmpdir, "rasterTest")))
+  cs <- read.table(file.path(tmpdir, "CHECKSUMS.txt"), header = T)
+  expect_true(NROW(cs) == 1)
+  expect_true(identical(cs$file, "rasterTest"))
+
 
 })
 
