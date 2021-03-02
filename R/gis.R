a912fdbc5f7508c24d499a9ebe2a2496f03514be
Eliot <eliotmci@mail.ubc.ca>
2021-03-02 08:50:25 -0800

    setTimeLimit fix

--- R/gis.R
+++ R/gis.R
@@ -296,8 +296,12 @@ findGDAL <- function() {
 }
 
 setGDALInst <- function(gdalPath) {
-  if (isWindows())
+  if (isWindows()) {
     setTimeLimit(20, transient = TRUE)
+    on.exit({
+      setTimeLimit(cpu = Inf, elapsed = Inf, transient = FALSE)
+    })
+  }
   out <- gdalUtils::gdal_setInstallation(gdalPath)
   return(out)
 }
