d0c654ca81c703c975f5e5b009582c4e624a82cd
Eliot McIntire <eliotmcintire@gmail.com>
2021-06-16 14:45:01 -0700

    some weird duplication of showSimilar messaging

--- R/cache.R
+++ R/cache.R
@@ -1534,7 +1534,8 @@ CacheDigest <- function(objsToDigest, algo = "xxhash64", calledFrom = "Cache", q
 
     if (sum(similar2[differs %in% TRUE]$differs, na.rm = TRUE)) {
       differed <- TRUE
-      messageCache("    ... because of (a) different ", paste(similar2[differs %in% TRUE]$fun, collapse = ", "),
+      messageCache("    ... because of (a) different ",
+                   unique(paste(similar2[differs %in% TRUE]$fun, collapse = ", ")),
                    verbose = verbose)
     }
 
