24f962405c64f87e472b538d97089590e7889352
Alex Chubaty <achubaty@for-cast.ca>
2021-08-06 13:01:15 -0600

    with prev

--- tests/testthat/test-prepInputs.R
+++ tests/testthat/test-prepInputs.R
@@ -2,7 +2,7 @@ test_that("prepInputs doesn't work (part 1)", {
   skip_on_cran()
   skip_on_ci()
 
-  testInitOut <- testInit(c("sf", "sp", "raster"), opts = list(
+  testInitOut <- testInit("raster", opts = list(
     "rasterTmpDir" = tempdir2(rndstr(1,6)),
     "reproducible.inputPaths" = NULL,
     "reproducible.overwrite" = TRUE)
