517562f970f9f0738a27fb986982e42de16f64a0
Eliot McIntire <eliotmcintire@gmail.com>
2022-01-27 13:46:17 -0800

    unit test updates for Cached spatRaster

--- tests/testthat/test-gis.R
+++ tests/testthat/test-gis.R
@@ -173,10 +173,22 @@ test_that("testing terra", {
   }, add = TRUE)
   if (requireNamespace("terra")) {
     f <- system.file("ex/elev.tif", package="terra")
-    tf <- tempfile()
+    tf <- tempfile(fileext = ".tif")
+    tf1 <- tempfile(fileext = ".tif")
+    tf2 <- tempfile(fileext = ".tif")
+    tf3 <- tempfile(fileext = ".tif")
+    tf4 <- tempfile(fileext = ".tif")
     file.copy(f, tf)
+    file.copy(f, tf1)
+    file.copy(f, tf2)
+    file.copy(f, tf3)
+    file.copy(f, tf4)
     r <- list(terra::rast(f), terra::rast(tf))
-    r1 <- list(terra::rast(f), terra::rast(tf))
+    r1 <- list(terra::rast(tf1), terra::rast(tf2))
+    r2 <- list(terra::rast(tf3), terra::rast(tf4))
+    rmem <- r2
+    terra::values(rmem[[1]]) <- terra::values(rmem[[1]])
+    terra::values(rmem[[2]]) <- terra::values(rmem[[2]])
 
     fn <- function(listOf) {
       listOf
@@ -200,17 +212,26 @@ test_that("testing terra", {
     expect_true(is(b[[1]], "SpatRaster"))
     expect_true(is(b[[2]][[1]], "SpatRaster"))
 
+    # mix memory and disk
+    b <- Cache(fn, list(r[[1]], r1, rmem))
+    expect_true(is(b, "list"))
+    expect_true(is(b[[1]], "SpatRaster"))
+    expect_true(is(b[[2]][[1]], "SpatRaster"))
+    expect_true(terra::inMemory(b[[3]][[1]]))
+    expect_true(!terra::inMemory(b[[2]][[1]]))
+    expect_true(!terra::inMemory(b[[1]]))
+
 
 
 
     f <- system.file("ex/lux.shp", package="terra")
-    v <- vect(f)
+    v <- terra::vect(f)
     v <- v[1:2,]
     rf <- system.file("ex/elev.tif", package="terra")
     xOrig <- terra::rast(rf)
     x <- xOrig
-    xCut <- classify(xOrig, rcl = 5)
-    xVect <- as.polygons(xCut)
+    xCut <- terra::classify(xOrig, rcl = 5)
+    xVect <- terra::as.polygons(xCut)
 
     y <- copy(x)
     y[y > 200 & y < 300] <- NA
@@ -228,7 +249,7 @@ test_that("testing terra", {
     expect_true(all.equal(t8, t1))
 
     t9 <- postProcessTerra(x, cropTo = vRast)
-    expect_true(ext(v) <= ext(t9))
+    expect_true(terra::ext(v) <= terra::ext(t9))
 
 
     # SR, SV
@@ -236,10 +257,10 @@ test_that("testing terra", {
 
     # No crop
     t3 <- postProcessTerra(x, maskTo = v)
-    expect_true(ext(t3) == ext(x))
+    expect_true(terra::ext(t3) == terra::ext(x))
 
     t4 <- postProcessTerra(x, cropTo = v, maskTo = v)
-    expect_true(ext(t4) == ext(t2))
+    expect_true(terra::ext(t4) == terra::ext(t2))
 
     t5 <- postProcessTerra(x, cropTo = v, maskTo = v, projectTo = v)
     expect_true(identical(t5[],t2[]))
@@ -253,30 +274,30 @@ test_that("testing terra", {
     ################
 
     t10 <- postProcessTerra(xVect, v)
-    expect_true(ext(t10) < ext(xVect))
+    expect_true(terra::ext(t10) < terra::ext(xVect))
 
     ################
     tf1 <- tempfile(fileext = ".shp")
     t11 <- postProcessTerra(xVect, v, writeTo = tf1)
-    vv <- vect(tf1)
+    vv <- terra::vect(tf1)
     expect_identical(terra::wrap(vv), terra::wrap(t11))
 
     # Test fixErrorTerra
     v1 <- terra::simplify(v)
-    gv1 <- geom(v1)
+    gv1 <- terra::geom(v1)
     gv1[gv1[, "geom"] == 2, "geom"] <- 1
     # gv1[9,"y"] <- 51
-    v2 <- vect(gv1, "polygons")
+    v2 <- terra::vect(gv1, "polygons")
     # plot(v2)
     # v2 <- is.valid(v2)
 
-    crs(v2) <- crs(v)
+    terra::crs(v2) <- terra::crs(v)
     t10 <- try(postProcessTerra(xVect, v2))
     expect_true(!is(t10, "try-error"))
 
     # Projection -->
     albers <- sf::st_crs("epsg:5070")$wkt
-    valbers <- project(v, albers)
+    valbers <- terra::project(v, albers)
 
 
     t11 <- postProcessTerra(x, valbers)
@@ -287,7 +308,7 @@ test_that("testing terra", {
     expect_true(sf::st_crs(t12) != sf::st_crs(valbers))
 
     # projection with errors
-    valbersErrors <- project(v2, albers)
+    valbersErrors <- terra::project(v2, albers)
     mess <- capture_messages(t13 <- postProcessTerra(xVect, valbersErrors))
     expect_true(sum(grepl("error", mess)) == 2)
     expect_true(sum(grepl("fixed", mess)) == 2)
