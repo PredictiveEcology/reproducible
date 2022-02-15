baec9a31930a8e8f50cecbb0f6e988cfec63cc42
Eliot McIntire <eliotmcintire@gmail.com>
2022-02-14 20:29:43 -0800

    whoops

--- R/robustDigest.R
+++ R/robustDigest.R
@@ -144,7 +144,7 @@ setMethod(
     }
 
     if (any(inherits(object, "SpatVector"), inherits(object, "SpatRaster"))) {
-      if (!requireNamespace("terra") && getOption("reproducible.useTerra", FALSE)) {
+      if (!requireNamespace("terra") && getOption("reproducible.useTerra", FALSE))
         stop("Please install terra package")
       out <- .doDigest(terra::wrap(object), algo)
       return(out)
