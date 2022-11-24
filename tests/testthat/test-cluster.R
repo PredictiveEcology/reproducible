ec50d833341ced7dc782637e343bba6c1fc7c44f
Eliot McIntire <eliotmcintire@gmail.com>
2022-11-24 13:03:25 -0800

    run test-cluster with more richness

--- tests/testthat/test-cluster.R
+++ tests/testthat/test-cluster.R
@@ -11,6 +11,7 @@ test_that("test parallel collisions", {
     # make cluster -- note this works if cluster is FORK also, but for simplicity, using default
     #   which works on Linux, Mac, Windows
     N <- min(2, detectCores())
+    N <- 10
 
     if (!file.exists(CacheDBFile(tmpdir))) {
       if (useDBI())
@@ -20,31 +21,45 @@ test_that("test parallel collisions", {
     # make function that will write to cache repository from with clusters
     fun <- function(x, cacheRepo) {
       #print(x)
-      Cache(rnorm, 10, sd = x, cacheRepo = cacheRepo)
+      reproducible::Cache(rnorm, 10, sd = x, cacheRepo = cacheRepo, verbose = 0)
     }
     # Run something that will write many times
     # This will produce "database is locked" on Windows or Linux *most* of the time without the fix
-    if (interactive()) {
-      of <- tmpfile[3]
-      cl <- makeCluster(N, outfile = of)
-      message(paste("log file is", of))
-    } else {
-      cl <- makeCluster(N)
-    }
-    on.exit(stopCluster(cl), add = TRUE)
+    # if (interactive()) {
+    #   of <- tmpfile[3]
+    #   cl <- makeCluster(N, outfile = of)
+    #   message(paste("log file is", of))
+    # } else {
+    cl <- makeCluster(N)
+    # }
+    # parallel::clusterEvalQ(cl, library(reproducible))
+    on.exit(try(stopCluster(cl), silent = TRUE), add = TRUE)
 
     clusterSetRNGStream(cl)
-#    parallel::clusterEvalQ(cl, {library(reproducible)})
-    numToRun <- 40
+    #    parallel::clusterEvalQ(cl, {library(reproducible)})
+    numToRun <- 400
+    x <- sample(1:10, size = numToRun, replace = TRUE)
 
     # There is a 'creating Cache at the same time' problem -- haven't resolved
     #  Just make cache first and it seems fine
-    Cache(rnorm, 1, cacheRepo = tmpdir)
-    a <- try(clusterMap(cl = cl, fun, seq(numToRun), cacheRepo = tmpdir, .scheduling = "dynamic"),
-             silent = FALSE)
+    # lapply(1:10, fun, cacheRepo = tmpdir)
+    st1 <- system.time(
+      a <- try(clusterMap(cl = cl, fun, x, cacheRepo = tmpdir, .scheduling = "dynamic"),
+               silent = FALSE))
+
+    st2 <- system.time(b <- try(lapply(x, fun, cacheRepo = tmpdir),
+                                silent = FALSE))
+    # Now do again with read only so that we can compare actual outputs
+    st1 <- system.time(
+      d <- try(clusterMap(cl = cl, fun, x, cacheRepo = tmpdir, .scheduling = "dynamic"),
+               silent = FALSE))
     if (!is(a, "try-error")) {
       expect_true(is.list(a))
       expect_true(length(a) == numToRun)
+      expect_true(all.equal(d, b))
+      # expect_true( 2*(st1[["elapsed"]]) < st2[["elapsed"]])
+
     }
+    stopCluster(cl)
   }
 })
