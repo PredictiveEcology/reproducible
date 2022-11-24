bf7e461e3c967260a8aa1f42d84c5c5ebb1c584c
Eliot McIntire <eliotmcintire@gmail.com>
2022-11-24 13:03:11 -0800

    minize Require imports

--- R/reexports.R
+++ R/reexports.R
@@ -1,11 +1,3 @@
-#' @importFrom Require Require
-#' @export
-Require::Require
-
-#' @importFrom Require pkgDep
-#' @export
-Require::pkgDep
-
 #' @importFrom Require checkPath
 #' @export
 Require::checkPath
@@ -13,14 +5,6 @@ Require::checkPath
 #' @importFrom Require normPath
 #' @export
 Require::normPath
-
-#' @importFrom Require trimVersionNumber
-#' @export
-Require::trimVersionNumber
-
-#' @importFrom Require tempfile2
-#' @export
-Require::tempfile2
 
 #' @importFrom Require tempdir2
 #' @export
