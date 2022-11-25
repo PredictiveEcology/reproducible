14e7f9db82657aee5d668265b25feec6d401a40b
Alex Chubaty <achubaty@for-cast.ca>
2022-11-24 22:42:43 -0700

    export tempdir2

--- R/paths.R
+++ R/paths.R
@@ -200,8 +200,18 @@ setMethod(
   }
 )
 
-
-
+#' Make a temporary (sub-)directory
+#'
+#' Create a temporary subdirectory in `.reproducibleTempPath()`, or a
+#' temporary file in that temporary subdirectory.
+#'
+#' @param sub Character string, length 1. Can be a result of
+#'   `file.path("smth", "smth2")` for nested temporary subdirectories.
+#' @param tempdir Optional character string where the
+#'   temporary dir should be placed. Defaults to `.reproducibleTempPath()`.
+#' @param create Logical. Should the directory be created. Default `TRUE`.
+#'
+#' @export
 tempdir2 <- function(sub = "",
                      tempdir = getOption("reproducible.tempPath", .reproducibleTempPath()),
                      create = TRUE) {
