c63b350a712fc4a7afc30b981dbf8ea886bb773c
Eliot McIntire <eliotmcintire@gmail.com>
2022-11-24 19:36:29 -0800

    remove examples from defunct git functions

--- R/git.R
+++ R/git.R
@@ -31,34 +31,7 @@
 #'
 #' @author Eliot McIntire and Alex Chubaty
 #' @export
-#' @importFrom utils getFromNamespace
 #' @rdname checkoutVersion
-#'
-#' @examples
-#' \dontrun{
-#'   tmpDir <- tempfile("")
-#'   dir.create(tmpDir)
-#'   repo <- "PredictiveEcology/reproducible"
-#'
-#'   ## get latest from master branch
-#'   localRepo <- checkoutVersion("PredictiveEcology/reproducible",
-#'                                localRepoPath = tmpDir)
-#'   git2r::summary(localRepo)
-#'   unlink(tmpDir, recursive = TRUE)
-#'
-#'   ## get latest from development branch
-#'   localRepo <- checkoutVersion(paste0(repo, "@", "development"), localRepoPath = tmpDir)
-#'   git2r::summary(localRepo)
-#'   unlink(tmpDir, recursive = TRUE)
-#'
-#'   ## get a particular commit by sha
-#'   sha <- "8179e1910e7c617fdeacad0f9d81323e6aad57c3"
-#'   localRepo <- checkoutVersion(paste0(repo, "@", sha), localRepoPath = tmpDir)
-#'   git2r::summary(localRepo)
-#'   unlink(tmpDir, recursive = TRUE)
-#'
-#'   rm(localRepo, repo)
-#' }
 #'
 checkoutVersion <- function(repo, localRepoPath = ".", cred = "", ...) {
   .Defunct(msg = "checkoutVersions is not sufficiently tested or developed to be useful")
