3924f3da10ba35a3de78716b8d1a48274533e2e4
Eliot McIntire <eliotmcintire@gmail.com>
2023-03-03 09:28:48 -0800

    findSimilar tweak for fn name changes

--- R/cache.R
+++ R/cache.R
@@ -1728,11 +1728,17 @@ CacheDigest <- function(objsToDigest, ..., algo = "xxhash64", calledFrom = "Cach
                    fnTxt,
                    "differs from", verbose = verbose)
     }
-    simFun <- similar$tagValue[similar$tagKey == "function"]
-    if (!identical(simFun, functionName))
-      fnTxt <- paste0("(whose function name was '", simFun, "')")
-    messageCache(paste0("    the next closest cacheId ", cacheIdOfSimilar, " ",
-                        fnTxt), verbose = verbose)
+
+    simFun <- similar[tagKey == "function", list(funName = tail(tagValue, 1)), by = cacheId]
+
+    # simFun <- similar$tagValue[similar$tagKey == "function"]
+    sameNames <- simFun$funName %in% functionName
+    if (!all(sameNames)) {
+      fnTxt <- paste0("(whose function name(s) was/were '", paste(simFun$funName, collapse = "', '"), "')")
+      messageCache(paste0("    the next closest cacheId(s) ", paste(cacheIdOfSimilar, collapse = ", "), " ",
+                          fnTxt, sep = "\n"), verbose = verbose)
+    }
+
 
     if (sum(similar2[differs %in% TRUE]$differs, na.rm = TRUE)) {
       differed <- TRUE
