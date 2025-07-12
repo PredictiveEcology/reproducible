6de6bce9a9d31d1e874e50798d140970b28cc9c6
Alex Chubaty <achubaty@for-cast.ca>
2025-07-11 21:17:47 -0600

    add cran only revdeps checks

--- revdep/check.R
+++ revdep/check.R
@@ -1,35 +1,55 @@
 ## install separately; keep this order to avoid conflicts
-# pak::pkg_install("fs")
+# pak::pkg_install(c("fs", "rprojroot"))
 # pak::pkg_install("HenrikBengtsson/revdepcheck.extras")
 # pak::pkg_install("r-lib/revdepcheck")
 # pak::pkg_install("achubaty/crancache@r-universe")
 
-options(
-  repos = c(
-    PE = "https://predictiveecology.r-universe.dev",
+CRAN_ONLY <- TRUE
 
-    ## note this is counter to the "Canonical CRAN.r-project.org" for CRAN packages,
-    ## but revdep/ directory is .Rbuildignored and this is used for revdep checks only.
-    CRAN = paste0("https://", "cloud.", "r-project.", "org")
-  ),
-  revdepcheck.num_workers = getOption("Ncpus", 2L)
-)
+if (isTRUE(CRAN_ONLY)) {
+  options(
+    repos = c(
+      ## note this is counter to the "Canonical CRAN.r-project.org" for CRAN packages,
+      ## but revdep/ directory is .Rbuildignored and this is used for revdep checks only.
+      CRAN = paste0("https://", "cloud.", "r-project.", "org")
+    ),
+    revdepcheck.num_workers = getOption("Ncpus", 2L)
+  )
 
-omit_pkgs <- c(
-  "SpaDES.project" ## omit b/c it circularly Suggests SpaDES.config
-)
-revdeps <- c(
-  revdepcheck.extras::revdep_children(),
-  revdepcheck.extras::revdep_grandchildren()
-) |>
-  setdiff(omit_pkgs)
+  revdeps <- rprojroot::find_package_root_file() |>
+    fs::path_file() |>
+    tools::package_dependencies(which = "most", recursive = FALSE, reverse = TRUE) |>
+    unlist() |>
+    unname()
+} else {
+  options(
+    repos = c(
+      PE = "https://predictiveecology.r-universe.dev",
+
+      ## note this is counter to the "Canonical CRAN.r-project.org" for CRAN packages,
+      ## but revdep/ directory is .Rbuildignored and this is used for revdep checks only.
+      CRAN = paste0("https://", "cloud.", "r-project.", "org")
+    ),
+    revdepcheck.num_workers = getOption("Ncpus", 2L)
+  )
+
+  omit_pkgs <- c(
+    "SpaDES.project" ## omit b/c it circularly Suggests SpaDES.config
+  )
+
+  revdeps <- c(
+    revdepcheck.extras::revdep_children(),
+    revdepcheck.extras::revdep_grandchildren()
+  ) |>
+    setdiff(omit_pkgs)
+}
 
 ## reset from any previous runs
 revdepcheck.extras::revdep_reset() ## clears revdep/{cache,checks,library} directories
 
 ## manual pre-installation (uses crancache)
-crancache::update_packages(lib.loc = .libPaths()[1], ask = FALSE)
-revdepcheck.extras::revdep_precache(pkgs = revdeps)
+# crancache::update_packages(lib.loc = .libPaths()[1], ask = FALSE)
+# revdepcheck.extras::revdep_precache(pkgs = revdeps)
 
 ## sequentially install deps of each revdeps package in parallel;
 ## NOTE: PE pkgs aren't crancached, and will be installed each time.
